cd36ae6aaf67679cab9b83bc6d09b352046a5d51
Kostiantyn <105097948+KonstantinKed@users.noreply.github.com>
2022-05-08 18:18:36 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,14 @@
+# TASK 2
+print ("Hello", "BEETROOT", "academy", sep='!!!')
+print ("Hello", "BEETROOT", "academy", sep='\t')
+print ("Hello", "BEETROOT", "academy", sep='\r')
+print ("Hello", "BEETROOT", "academy", sep='\n')
+print(1,3,5,7,11, sep=',,,')
+print()
+# COMMENTS PRACTICE
+# print ("Hello", "BEETROOT", "academy", sep='!!!')
+# print(1,3,5,7,11, sep=',,,')
+
+# TASK 3
+print ("#########", "#\t\t#", "#\t\t#", "#\t\t#", "#\t\t#", "#########", sep="\n", end="\n\n")
+print ("#\t\t#", "#\t\t#", "#\t\t#", "#########", "#\t\t#", "#\t\t#", "#\t\t#", sep="\n")
