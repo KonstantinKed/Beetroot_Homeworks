ec4b116acba5f3284824cf7ee77add40e2a2ec77
KonstantinKed <knkostya@gmail.com>
2022-06-12 17:01:07 +0300

    Updated task 15_add_1

--- Task15/Task15_add_1.py
+++ Task15/Task15_add_1.py
@@ -15,6 +15,8 @@ class Parcel:
         self.length = length
         self.height = height
         Parcel.parcel_amount += 1
+        if Parcel.parcel_amount > 5:
+            print("You exceed maximum parcel amount!!!")
 
     def parcel_stock(self):
         Parcel.stock.append([self.width,self.length, self.height])
@@ -24,17 +26,24 @@ class Parcel:
             Parcel.stock_value.append(reduce(lambda a, b: a * b, i))
 
     def smallest_size(self):
-        print(min(x for x in Parcel.stock_value))
+        print("Smallest parcel in stock is:",min(x for x in Parcel.stock_value),"m3")
+
+
 
 p1 = Parcel(5,6,1)
 p2 = Parcel(2,4,3)
 p3 = Parcel(8,4,1)
-
+p4 = Parcel(1,4,1)
+p5 = Parcel(1,4,8)
+p6 = Parcel(2,2,4)
 p1.parcel_stock()
 p2.parcel_stock()
 p3.parcel_stock()
-print(Parcel.parcel_amount)
-print(Parcel.stock)
+p4.parcel_stock()
+p5.parcel_stock()
+p6.parcel_stock()
+print("Total parcels in stock:",Parcel.parcel_amount)
+print("Parcels sizes in stock:",Parcel.stock)
 p1.parcel_size()
-print(Parcel.stock_value)
+print("Volume of each parcel in stock:",Parcel.stock_value)
 p1.smallest_size()
