abd10a16495472a129f5d39648e3fdad15d96689
KonstantinKed <knkostya@gmail.com>
2022-07-10 22:48:29 +0300

    Home task 17_3 Fixed and updated

--- Task17/Task17_3.py
+++ Task17/Task17_3.py
@@ -1,26 +1,44 @@
-from fractions import Fraction
-
+from math import gcd
 class Fractions:
-    def __init__(self, x):
-        self.x = x
+    def __init__(self, n, d):
+        self.n = n
+        self.d = d
+
+    def big_com_div(self, a, b):
+        if a == 1:
+            return a, b
+        else:
+            a, b = int(a/gcd(a,b)), int(b/gcd(a,b))
+            if b == 1:
+                return a
+            else:
+                return a, b
 
     def __add__(self, other):
-        return self.x + other.x
+        nn = self.n * other.d + other.n * self.d
+        dd = self.d * other.d
+        return self.big_com_div(nn,dd)
 
     def __sub__(self, other):
-        return self.x - other.x
+        nn = self.n * other.d - other.n * self.d
+        dd = self.d * other.d  #
+        return self.big_com_div(nn,dd)
 
     def __mul__(self, other):
-        return self.x * other.x
+        nn = self.n * other.n
+        dd = self.d * other.d  #
+        return self.big_com_div(nn,dd)
 
     def __truediv__(self, other):
-        if other.x == 0:
+        if other.n == 0 or other.d == 0:
             raise ZeroDivisionError ("can't divide on 0")
-        return self.x / other.x
+        nn = self.n * other.d
+        dd = self.d * other.n
+        return self.big_com_div(nn,dd)
 
-f1 = Fractions(1/2)
-f2 = Fractions(1/4)
-print(Fraction(f1+f2))
-print(Fraction(f1-f2))
-print(Fraction(f1*f2))
-print(Fraction(f1/f2))
+f1 = Fractions(1, 2)
+f2 = Fractions(1, 4)
+print((f1+f2))
+print((f1-f2))
+print((f1*f2))
+print((f1/f2))
