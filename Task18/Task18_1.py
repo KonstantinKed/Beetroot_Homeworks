84330d52a9b8c244522a6c4762adbc2a61bb163a
KonstantinKed <knkostya@gmail.com>
2022-07-10 18:41:07 +0300

    Home task 18_1 Updated

--- Task18/Task18_1.py
+++ Task18/Task18_1.py
@@ -2,33 +2,36 @@
 # passed to the constructor. The logic inside the `validate` method could be to check if the passed email parameter is a valid email string.
 import re
 class Email:
-    def __init__(self, email):
-        self.email = email
+    def __init__(self):
+        pass
 
-    @classmethod
-    def validate(Email, email):
+
+    def validate(self, email):
         if re.match("^[a-z0-9]+[\._-]?[a-z0-9]+[@]\w+[.]\w{2,3}$", email):
             return (f"{email} -- is correct syntax in email address")
         else:
             return (f"{email} -- is Wrong syntax in email address")
 
 
-print(Email.validate(("abc..def@mail.com")))
-print(Email.validate(("abc-@mail.com")))
-print(Email.validate((".abc@mail.com")))
-print(Email.validate(("abc#def@mail.com")))
-print(Email.validate(("abc.def@mail.c")))
-print(Email.validate(("abc.def@mail#archive.com")))
-print(Email.validate(("abc.def@mail")))
-print(Email.validate(("abc.def@mail..com")))
-print(Email.validate(("abc.def@mail.com")))
-print(Email.validate(("abc.def@mail.org")))
-print(Email.validate(("abc.def@mail-archive.com")))  # counts as wrong =(  need some check in regex list
-print(Email.validate(("abc.def@mail.cc")))
-print(Email.validate(("abc_def@mail.com")))
-print(Email.validate(("abc@mail.com")))
-print(Email.validate(("abc.def@mail.com")))
-print(Email.validate(("abc-d@mail.com")))
+
+em = Email()
+
+print(em.validate(("abc..def@mail.com")))
+print(em.validate(("abc-@mail.com")))
+print(em.validate((".abc@mail.com")))
+print(em.validate(("abc#def@mail.com")))
+print(em.validate(("abc.def@mail.c")))
+print(em.validate(("abc.def@mail#archive.com")))
+print(em.validate(("abc.def@mail")))
+print(em.validate(("abc.def@mail..com")))
+print(em.validate(("abc.def@mail.com")))
+print(em.validate(("abc.def@mail.org")))
+print(em.validate(("abc.def@mail-archive.com")))  # counts as wrong =(  need some check in regex list
+print(em.validate(("abc.def@mail.cc")))
+print(em.validate(("abc_def@mail.com")))
+print(em.validate(("abc@mail.com")))
+print(em.validate(("abc.def@mail.com")))
+print(em.validate(("abc-d@mail.com")))
 
 
 
